c3c38f12e4a6040d265f1bf3a9251e829eddb3d3
Abdul-ur-Rehman <chattha926242@gmail.com>
2025-07-20 12:44:30 +0500

    Count Vowel

--- CountVowelsInString.py
+++ CountVowelsInString.py
@@ -1,2 +1,23 @@
-n =5
-print(range(n-1))
+def countVowel(word):
+    word_length = len(word)
+    count = 0
+    for i in range(word_length):
+        word = word.lower()
+        if word[i] == "a" or word[i] == "e" or  word[i] == "i" or word[i] == "o" or word[i] == "u":
+            count += 1
+    return count
+
+# Simpler version
+
+def simple_countVowel(word):
+    word = word.lower()
+    count = 0
+    for char in word:
+        if char in "aeiou":
+            count += 1
+    return count
+
+
+word = "Abdul-ur-Rehman"
+print(countVowel(word))
+print(simple_countVowel(word))
