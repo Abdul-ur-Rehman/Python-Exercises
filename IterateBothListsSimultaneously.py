3d70d2bc7de6c76c5616e64c9a86b2135dd4cbf7
Abdul-ur-Rehman <chattha926242@gmail.com>
2025-07-24 05:33:30 +0500

    IterateBothListsSimultaneously

---
+++ IterateBothListsSimultaneously.py
@@ -0,0 +1,8 @@
+def iterateSimultaneously(list1, list2):
+
+    for i in range(len(list2)):
+        print(list1[i], list2[i])
+
+list1 = [10, 20, 30, 40]
+list2 = [100, 200, 300, 400]
+iterateSimultaneously(list1, list2)
