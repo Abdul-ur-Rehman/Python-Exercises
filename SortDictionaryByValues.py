4bff0245c37db604e7cf67606e2e86a5e81e90bc
Abdul-ur-Rehman <chattha926242@gmail.com>
2025-07-25 11:56:56 +0500

    SortDictionaryByValues

---
+++ SortDictionaryByValues.py
@@ -0,0 +1,15 @@
+def sortDict(myDict):
+
+    itemsList = list(my_dict.items())
+    sortedValues = {}
+    for i in range(len(itemsList)):
+        for j in range(i+1, len(itemsList)):
+            if itemsList[i][1] > itemsList[j][1]:
+                itemsList[i], itemsList[j] = itemsList[j], itemsList[i]
+
+
+    return dict(itemsList)
+
+my_dict = {'Jessa': 3, 'Kelly': 1, 'Jon': 2, 'Kerry': 4, 'Joy': 1}
+
+print(sortDict(my_dict))
