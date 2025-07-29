5117daa78b14e2e6e29dc9627143994f81574945
Abdul-ur-Rehman <chattha926242@gmail.com>
2025-07-29 11:34:01 +0500

    CountWordsFromFile

--- Basics/CountWordsFromFile.py
+++ Basics/CountWordsFromFile.py
@@ -1,10 +1,14 @@
 try:
-    with open("sample.txt", "r") as file:
-        content = file.read()
-        words = content.split()
-        total_words = len(words)
+    def countWords(filename):
+        with open(filename, "r") as file:
+            content = file.read()
+            words = content.split()
+            total_words = len(words)
 
-        print(f"File has {total_words} total words.")
+            print(f"File has {total_words} total words.")
 
 except FileNotFoundError:
     print("File Not Found!")
+
+filename = "sample.txt"
+countWords(filename)
