0d218a233e6668179c1da6ee3e4fe3e663262e00
Abdul-ur-Rehman <chattha926242@gmail.com>
2025-07-29 12:39:12 +0500

    WriteToFile

---
+++ Basics/WriteToFile.py
@@ -0,0 +1,16 @@
+def writeToFile(filename,output):
+    with open(filename, "r") as reader:
+        content = reader.readlines()
+        print("Original Content ==>", content)
+        reversed_content = content[::-1]
+        print("Reversed Content ==>", reversed_content)
+
+    with open(output, "w") as writer:
+        for line in reversed_content:
+            writer.write(line.rstrip() + "\n")
+
+        print("Content is written on file.")
+
+filename = "sample.txt"
+output = "output.txt"
+writeToFile(filename, output)
