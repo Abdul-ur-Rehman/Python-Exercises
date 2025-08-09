6ff6ed99e7a9e4e4ea77d85aa0c1901aeeff63ec
Abdul-ur-Rehman <chattha926242@gmail.com>
2025-08-09 13:09:46 +0500

    multiple test datasets for single testcase

--- PrintCellOfExcel.py
+++ PrintCellOfExcel.py
@@ -11,11 +11,13 @@ print(sheet.max_column)
 #sheet.cell(row=2, column=2).value = "Abdur Rehman"
 print(sheet.cell(row=2, column=2).value)
 Dict = {}
+list = []
 
 for i in range(1, sheet.max_row + 1):
     if sheet.cell(row=i, column=1).value == "test_HomePage_FillingForm":
         for j in range(2, sheet.max_column + 1):
             Dict[sheet.cell(row=1, column=j).value] = sheet.cell(row=i, column=j).value
 
+        list.append(Dict)
 
-print(Dict)
+print(list)
