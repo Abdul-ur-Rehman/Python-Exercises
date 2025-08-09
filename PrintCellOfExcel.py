581c6f0fdde54cf31b619b19fce5ec1c42198b9b
Abdul-ur-Rehman <chattha926242@gmail.com>
2025-08-09 12:59:14 +0500

    extracting data from excel for a specific test case

--- PrintCellOfExcel.py
+++ PrintCellOfExcel.py
@@ -10,8 +10,12 @@ print(sheet.max_column)
 
 #sheet.cell(row=2, column=2).value = "Abdur Rehman"
 print(sheet.cell(row=2, column=2).value)
+Dict = {}
 
-for i in range(1, sheet.max_row+1):
-    if sheet.cell(row=i, column=1).value == "Test6":
-        for j in range(1, sheet.max_column+1):
-            print(sheet.cell(row=i, column=j).value, end=" ")
+for i in range(1, sheet.max_row + 1):
+    if sheet.cell(row=i, column=1).value == "test_HomePage_FillingForm":
+        for j in range(2, sheet.max_column + 1):
+            Dict[sheet.cell(row=1, column=j).value] = sheet.cell(row=i, column=j).value
+
+
+print(Dict)
